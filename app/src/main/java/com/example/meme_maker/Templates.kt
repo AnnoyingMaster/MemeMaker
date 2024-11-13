cd8dbfa69d4efaf28818fcd993f884ebd2e3b474
AnnoyingMaster <151389995+AnnoyingMaster@users.noreply.github.com>
2024-11-13 21:05:46 +0100

    Database features
    Hozzáadva:
    Entity classok és Dao Interfészek

---
+++ app/src/main/java/com/example/meme_maker/Templates.kt
@@ -0,0 +1,15 @@
+package com.example.meme_maker
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.Date
+
+@Entity(tableName = "template_table")
+data class Templates(
+    @PrimaryKey(autoGenerate = true) val id : Int,
+    @ColumnInfo(name = "file_name") val name : String,
+    @ColumnInfo(name = "file_path") val path : String,
+    @ColumnInfo(name="created_at") val creationDate : Date,
+    @ColumnInfo(name="default_template") val default : Int,
+)
