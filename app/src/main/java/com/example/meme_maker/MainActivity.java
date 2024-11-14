a8c9bd6f3da99a08a5053dcc57916f0d38559092
BBence0125 <bacsikbence94@gmail.com>
2024-11-14 22:20:10 +0100

    Commit1

--- app/src/main/java/com/example/meme_maker/MainActivity.java
+++ app/src/main/java/com/example/meme_maker/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.meme_maker;
 
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
@@ -17,11 +24,27 @@ public class MainActivity extends AppCompatActivity {
 
 //TODO: Meg kell csinálni dik : D 
 
+    private static int RESULT_LOAD_IMAGE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Button buttonLoadImage = (Button) findViewById(R.id.buttonLoadPicture);
+        buttonLoadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
+
+
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -35,7 +58,27 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+       super.onActivityResult(requestCode, resultCode, data);
 
+       if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data){
+           Uri selectedImage = data.getData();
+           String[] filePathColumn = { MediaStore.Images.Media.DATA};
+
+           Cursor cursor = getContentResolver().query(selectedImage,
+                   filePathColumn, null, null, null, null);
+           cursor.moveToFirst();
+
+           int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+           String picturePath = cursor.getString(columnIndex);
+           cursor.close();
+
+           ImageView imageView = (ImageView) findViewById(R.id.memeImageView);
+           imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+       }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -43,8 +86,8 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-   @Override
-   public boolean onOptionsItemSelected(MenuItem item) {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_home:
                 loadFragment(new Main(), "Főoldal");
@@ -59,7 +102,7 @@ public class MainActivity extends AppCompatActivity {
                 loadFragment(new GalleryFragment(), "Gallery");
                 return true;
         }
-   }
+    }
 
     private void showMessage(String message) {
         Toast.makeText(getApplicationContext(), message,
@@ -71,4 +114,11 @@ public class MainActivity extends AppCompatActivity {
                 getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.fragment_container, fragment, tag);
         fragmentTransaction.commit();
+    }
+
+
+
+
+
 }
+
