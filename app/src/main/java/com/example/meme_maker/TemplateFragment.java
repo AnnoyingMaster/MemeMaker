07a36e5cc54c7f86a088689c0383d0844d3b9742
MolnarBence <molnar.bence1028@gmail.com>
2024-11-18 13:10:23 +0100

    Gallery és template listta kepek megjelennek
    .

--- app/src/main/java/com/example/meme_maker/TemplateFragment.java
+++ app/src/main/java/com/example/meme_maker/TemplateFragment.java
@@ -1,24 +1,36 @@
 package com.example.meme_maker;
-
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class TemplateFragment extends Fragment {
 
+    // TODO: Customize parameter argument names
+    private static final String ARG_COLUMN_COUNT = "column-count";
+    // TODO: Customize parameters
+    private int mColumnCount = 1;
+
+    private List<Item> memeList;
+    private RecyclerView templateRecyclerView;
+    private TemplateItemRecyclerViewAdapter adapter;
 
     public TemplateFragment() {
-        // Required empty public constructor
     }
 
-
-    public static TemplateFragment newInstance(String param1, String param2) {
+    // TODO: Customize parameter initialization
+    @SuppressWarnings("unused")
+    public static TemplateFragment newInstance(int columnCount) {
         TemplateFragment fragment = new TemplateFragment();
-
+        Bundle args = new Bundle();
+        args.putInt(ARG_COLUMN_COUNT, columnCount);
+        fragment.setArguments(args);
         return fragment;
     }
 
@@ -26,12 +38,30 @@ public class TemplateFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        if (getArguments() != null) {
+            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
+        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_template, container, false);
+        View view = inflater.inflate(R.layout.fragment_gallery_list, container, false);
+        memeList = new ArrayList<>();
+
+        memeList.add(new Item( R.drawable.memetemplate_1, "Dog and Dawg" ));
+        memeList.add(new Item( R.drawable.memetemplate_1, "Dog and Dawg" ));
+        memeList.add(new Item( R.drawable.memetemplate_1, "Dog and Dawg" ));
+
+        templateRecyclerView = view.findViewById(R.id.templatesRecyclerView);
+        adapter = new TemplateItemRecyclerViewAdapter(memeList);
+        RecyclerView.LayoutManager layoutManager =
+                new LinearLayoutManager(getContext());
+        templateRecyclerView.setLayoutManager(layoutManager);
+        templateRecyclerView.setAdapter(adapter);
+
+
+      
+        return view;
     }
 }
