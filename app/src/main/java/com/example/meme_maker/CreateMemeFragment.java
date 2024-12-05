265134f52ac6b16b6b074b67726b60c255001c4e
AnnoyingMaster <151389995+AnnoyingMaster@users.noreply.github.com>
2024-12-05 21:58:32 +0100

    Merge1
    Gallery és Template branch tartalmának egyesítése a fájlkezelő branch tartalmával

--- app/src/main/java/com/example/meme_maker/CreateMemeFragment.java
+++ app/src/main/java/com/example/meme_maker/CreateMemeFragment.java
@@ -1,12 +1,32 @@
 package com.example.meme_maker;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 
 public class CreateMemeFragment extends Fragment {
@@ -16,15 +36,95 @@ public class CreateMemeFragment extends Fragment {
         // Required empty public constructor
     }
 
+    ImageButton buttonBrowse, buttonCamera;
+    ImageView testImgView;
+
+    private final int CAM_REQ = 1000;
+    private final int IMG_REQ = 2000;
+    Uri imageUri;
+
+
+
+    private ActivityResultLauncher<Intent> cameraLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    Bundle extras = result.getData().getExtras();
+                    Bitmap imageBitmap = (Bitmap) extras.get("data");
+                    testImgView.setImageBitmap(imageBitmap);
+                }
+            }
+    );
+
+    private ActivityResultLauncher<Intent> galleryLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    Uri imageUri = result.getData().getData();
+                    testImgView.setImageURI(imageUri);
+                }
+            }
+    );
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        buttonBrowse = view.findViewById(R.id.btnBrowse);
+        testImgView = view.findViewById(R.id.testImgView);
+        buttonCamera = view.findViewById(R.id.btnCamera);
+
+        buttonCamera.setOnClickListener(v -> {
+            Intent camIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            cameraLauncher.launch(camIntent);
+        });
+
+        buttonBrowse.setOnClickListener(v -> {
+            Intent photoIntent = new Intent(Intent.ACTION_PICK);
+            photoIntent.setType("image/*");
+            galleryLauncher.launch(photoIntent);
+        });
+
+
+
+
     }
+
+
+    private static final int RESULT_OK = 1;
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && data != null) {
+            if (requestCode == CAM_REQ) {
+                Bitmap camBitmap = (Bitmap) data.getExtras().get("data");
+                if (camBitmap != null) {
+                    testImgView.setImageBitmap(camBitmap);
+                }
+            } else if (requestCode == IMG_REQ) {
+                imageUri = data.getData();
+                if (imageUri != null) {
+                    testImgView.setImageURI(imageUri);
+                }
+            }
+        }
+    }
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_meme, container, false);
+        View view = inflater.inflate(R.layout.fragment_create_meme, container, false);
+
+        return view;
     }
+
+
+
+
+
 }
